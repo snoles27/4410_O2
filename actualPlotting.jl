d9395647c70f281c12432d7b709b79f3b60f4bdf
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-02-15 20:40:19 -0500

    added shaded rectange

--- actualPlotting.jl
+++ actualPlotting.jl
@@ -72,21 +72,71 @@ function plotFringes(λmid::Float64, λr::Float64, threshold::Float64)
 
 end
 
+function getBandWidths(counts, λr, minConut, maxCount)
+
+    bandwidths = zeros(0)
+
+    for i = 1:length(counts)
+
+        if counts[i] <= maxCount && counts[i] >= minConut
+            append!(bandwidths, λr[i])
+        end
+
+    end
+
+    return bandwidths
+
+end
+
 
 let
 
     λmid = 540.0 #nm
-    λr = range(100, 600, 1000)
+    λlow = 50
+    λhigh = 600
+    λr = range(λlow, λhigh, 3000)
 
     setup(bandWidth) = numFringes(λmid, bandWidth, .95)
 
     counts = setup.(λr)
 
+    #DATA
+    whiteCountMin = 19
+    whiteCountMax = 20
+
+    greenCountMin = 57
+    greenCountMax = 65
+
+    #setting up bandwidth calcs
+    bandwidthsWhite = getBandWidths(counts, λr, whiteCountMin, whiteCountMax)
+    bandwidthsGreen = getBandWidths(counts, λr, greenCountMin, greenCountMax)
+
     pygui(true)
     plot(counts, λr)
+    ax = gca()
+
+    # add new limits
+    ax[:set_xlim]([minimum(counts),maximum(counts)])
+    ax[:set_ylim]([λlow,λhigh])
+    y1_w = [minimum(bandwidthsWhite), minimum(bandwidthsWhite)]
+    y2_w = [maximum(bandwidthsWhite), maximum(bandwidthsWhite)]
+    x_w = [minimum(counts), whiteCountMax]
+    fill_between(x_w, y2_w, y1_w, where=(y1_w .< y2_w), color = "red", alpha = 0.3)
+
+    y1_g = [minimum(bandwidthsGreen), minimum(bandwidthsGreen)]
+    y2_g = [maximum(bandwidthsGreen), maximum(bandwidthsGreen)]
+    x_g = [minimum(counts), greenCountMax]
+    fill_between(x_g, y2_g, y1_g, where=(y1_g .< y2_g), color = "green", alpha = 0.3)
+
     ylabel("Bandwidth (nm)")
     xlabel("Fringe Count")
-    vlines([19,20], 100, 600, color = "red", label = "Observed Fringe Counts")
+    vlines([whiteCountMin,whiteCountMax], λlow, λhigh, color = "red", linestyle = "dashed", label = "Fringe Counts (White Light)")
+    vlines([greenCountMin,greenCountMax], λlow, λhigh, color = "green",linestyle = "dashed", label = "Fringe Counts (Green Light)")
+
+    
+    vlines([minimum(counts)], minimum(bandwidthsWhite), maximum(bandwidthsWhite), color = "red", linewidth = 5, label = "Bandwith Range (White Light)")
+    vlines([minimum(counts)], minimum(bandwidthsGreen), maximum(bandwidthsGreen), color = "green", linewidth = 5, label = "Bandwith Range (Green Light)")
+    
     legend()
 
     # plotFringes(λmid, 350.0, .95)
