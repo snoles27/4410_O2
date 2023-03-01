1c95a34a9f9d8e19416618c3a76b33bed45fe369
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-03-01 16:31:37 -0500

    smoothing function and scaling function
    data looking a bit tough

--- S7/calibrate.jl
+++ S7/calibrate.jl
@@ -1,5 +1,8 @@
 using CSV
 using DataFrames
+using PyPlot
+using Peaks
+using Statistics
 
 
 function pullNumber(input)
@@ -27,6 +30,36 @@ function getNeonData(file::String)
 
 end
 
+function getData(file::String)
+    data = CSV.read(file, DataFrame)
+    times = data[2:end-5,2]
+    V = data[2:end-5,3]
+
+    m = zeros(length(times), 2)
+
+    for i = 1:length(times)
+        m[i,1] = parse(Float64, times[i])
+        m[i,2] = parse(Float64, V[i])
+    end
+
+    return m
+    
+end
+
+function relIntenCutOff(relInten::Float64, data::Matrix{Float64})
+
+    m = Array{Float64}(undef, 0, 2)
+    compare = maximum(data[:,2]) * relInten
+
+    for i = 1:size(data)[1]
+        if data[i,2] >= compare
+            m = vcat(m, data[i,:]')
+        end
+    end
+    return m
+    
+end
+
 function wlen2wnum(wavelength::Float64)
     #wavelength: Wavelength in nm
     #returns: wavenumber in cm^-1
@@ -43,10 +76,94 @@ function wnum2wlen(wavenumber::Float64)
 
 end
 
+function smooth(data::Matrix{Float64}, averageNumber::Int)
+
+    dataNum = size(data)[1]
+    smoothed = zeros(dataNum,1)
+    lowEnd = round(Int, averageNumber/2)
+    highEnd = averageNumber - lowEnd
+
+
+
+    for i = 1:dataNum
+
+        if i < lowEnd + 1
+            bottom = 1
+            top = i + highEnd
+        elseif i > dataNum - highEnd
+            top = dataNum
+            bottom = i - lowEnd
+        else
+            bottom = i - lowEnd
+            top = i + highEnd
+        end
+
+        smoothed[i] = mean(data[bottom:top, 2])
+    end
+        
+    return hcat(data[:,1], smoothed)
+
+end
+
+function getPeaks(data::Matrix{Float64})
+
+    peakIndexs = argmaxima(data[:,2])
+
+    peaks = zeros(length(peakIndexs),2)
+
+    for (i, index) in enumerate(peakIndexs)
+        peaks[i,:] = data[index, :]
+    end
+    
+    return peaks
+
+end
+
+function rescaleX(data::Matrix{Float64}, min::Float64, max::Float64)
+
+    slope = (max - min)/(data[end,1] - data[1,1])
+
+    rescaled = min .+ slope * data[:,1]
+
+    return hcat(rescaled, data[:,2])
+
+end
+
 let 
 
-    data = getNeonData("S7/Ne_I.csv")
+    neondata = getNeonData("S7/Ne_I.csv")
+    shortenedData = relIntenCutOff(0.1, neondata)
 
+    data4 = getData("S7/run4.csv")
+    wnmin = 21770.0 #cm-1
+    wnmax = 22900.0 #cm-1
+
+    wlenmax = wnum2wlen(wnmin)
+    wlenmin = wnum2wlen(wnmax)
+
+    smoothed = smooth(data4, 30)
+    peaks = getPeaks(smoothed)
+    peaksCuttoff = relIntenCutOff(0.2, peaks)
+
+    scaleChange = rescaleX(smoothed, wlenmin, wlenmax)
+    scaleChange_raw = rescaleX(data4, wlenmin, wlenmax)
+
+
+
+    pygui(true)
+    plot(scaleChange_raw[:,1], scaleChange_raw[:,2])
+    plot(scaleChange[:,1], scaleChange[:,2])
+    for row in eachrow(shortenedData)
+        vlines(row[1], 1, 1 + row[2] * .01, color = "red")
+    end
+
+
+
+
+
+    # plot(data4[:,1], data4[:,2])
+    # plot(smoothed[:,1], smoothed[:,2])
+    # scatter(peaksCuttoff[:,1], peaksCuttoff[:,2], c = "red")
 
 
 end
